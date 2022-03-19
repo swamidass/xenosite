9a37167c58a17336584e2e3ba1da4d55782b16f5
S. Joshua Swamidass <swamidass@gmail.com>
2022-03-19 18:30:16 -0500

    rdkit

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,8 +1,24 @@
-export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Xenosite</h1>
+import initRDKitModule from '@rdkit/rdkit'
+import { useLoaderData } from 'remix'
 
+
+export async function loader() {
+	const rdkit = await initRDKitModule()
+	var svg = rdkit.get_mol("CCCC=O").get_svg()
+	return {svg}
+}
+
+
+export default function Index({svg}) {
+  var { svg } = useLoaderData();
+
+
+  return (
+	<div>
+    <div className="max-w-lg mx-auto pt-10 bg-gray-100">
+      <h1>Welcome to Xenosite</h1>
+      <div className=" w-full" dangerouslySetInnerHTML={{__html: svg}} />
     </div>
+</div>
   );
 }
