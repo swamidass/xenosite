b7510594292ade6ed93d16bacdca8daf6b7e1a6d
S. Joshua Swamidass <swamidass@gmail.com>
2022-03-19 13:58:39 -0400

    add tailwind

--- app/root.jsx
+++ app/root.jsx
@@ -15,6 +15,12 @@ export function meta() {
   };
 }
 
+import styles from "./styles/app.css"
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }]
+}
+
 export default function App() {
   return (
     <html lang="en">
