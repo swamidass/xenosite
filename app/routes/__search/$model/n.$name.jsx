cf49df73bca5c5c10ffc06642883fca9a975caf3
S. Joshua Swamidass <swamidass@gmail.com>
2022-10-08 02:28:12 -0500

    removed imports to fix vercel deploy

--- app/routes/__search/$model/n.$name.jsx
+++ app/routes/__search/$model/n.$name.jsx
@@ -1,1 +1,29 @@
-export { headers, loader, default } from "~/routes/__search/n.$name";
+import { useLoaderData, json } from "remix";
+import { resolve_query_as_name } from "~/search";
+import { ResultSummaryDisplay } from "~/components/ResultSummaryDisplay";
+
+export function headers() {
+  return {
+    "Cache-Control": "s-maxage=60, stale-while-revalidate=600",
+  };
+}
+
+export async function loader({ params }) {
+  const [response, resolved_name] = await resolve_query_as_name(
+    params.smiles,
+    params.model
+  );
+
+  return json(
+    {
+      params,
+      response,
+      resolved_name,
+    },
+    { headers: { "Cache-Control": "s-maxage=60, stale-while-revalidate=600" } }
+  );
+}
+
+export default function Model() {
+  return <ResultSummaryDisplay />;
+}
--- app/routes/__search/n.$name.jsx
+++ app/routes/__search/n.$name.jsx
@@ -1,11 +1,16 @@
-import { json } from "remix";
+import { useLoaderData, json } from "remix";
 import { resolve_query_as_name } from "~/search";
+import { ResultSummaryDisplay } from "~/components/ResultSummaryDisplay";
 
-export { default, headers } from "~/routes/__search/_.$smiles";
+export function headers() {
+  return {
+    "Cache-Control": "s-maxage=60, stale-while-revalidate=600",
+  };
+}
 
 export async function loader({ params }) {
   const [response, resolved_name] = await resolve_query_as_name(
-    params.name,
+    params.smiles,
     params.model
   );
 
@@ -17,4 +22,8 @@ export async function loader({ params }) {
     },
     { headers: { "Cache-Control": "s-maxage=60, stale-while-revalidate=600" } }
   );
+}
+
+export default function Model() {
+  return <ResultSummaryDisplay />;
 }
