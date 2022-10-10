d2953999e9a554d25742029f4260845612ddbb6c
S. Joshua Swamidass <swamidass@gmail.com>
2022-10-09 23:04:42 -0500

    Key front page information.

--- app/routes/__search/index.jsx
+++ app/routes/__search/index.jsx
@@ -9,11 +9,27 @@ export default function Model() {
         XenoSite predicts how small-molecules are metabolized by liver enzymes,
         and if they are prone to covalently attach to biological molecules.
       </p>
+      <p>
+        This website is free for all academic and evaluative use. Licenses are
+        available for secure commercial access and access to the programatic
+        API.
+      </p>
+      <p>
+        Tell us what you think. Post any comments, questions or problems on the{" "}
+        <a target="_blank" href="https://discourse.xenosite.org">
+          forum
+        </a>
+        .
+      </p>
 
       {MODELS.map((x) => (
         <Fragment key={x.path}>
           <h2>
-            <Link className="no-underline hover:underline" to={`/${x.path}`}>
+            <Link
+              className="no-underline hover:underline"
+              to={`/${x.path}`}
+              rel="prefetch"
+            >
               {x.model}
             </Link>
           </h2>
@@ -21,6 +37,18 @@ export default function Model() {
           {x.info ? <x.info /> : null}
         </Fragment>
       ))}
+
+      <h2>Coming soon...</h2>
+
+      <p>
+        {" "}
+        This website is under active development. More models will be available
+        soon. Tell us what to prioritize on the{" "}
+        <a target="_blank" href="https://discourse.xenosite.org">
+          forum
+        </a>
+        .
+      </p>
     </div>
   );
 }
