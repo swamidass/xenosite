87501950a2cfd0cbc0dc5e8ec655b97d799fd372
S. Joshua Swamidass <swamidass@gmail.com>
2022-10-09 22:50:23 -0500

    tailwind typography added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-  ],
-    theme: {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
